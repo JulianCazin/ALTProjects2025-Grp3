a43dd4033cc6020f1a4b392d127925afe977700f
Ra-bite <juliancazin.pro@gmail.com>
2025-09-02 18:50:00 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+
+logger = logging.getLogger(__name__)
+
+def main():
+    """Fonction principale de l'application."""
+    logger.info("Démarrage du programme...")
+    
+    print("Hello, world !")
+    
+    logger.info("Fin du programme.")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.exception("Une erreur est survenue: %s", e)
+        sys.exit(1)
